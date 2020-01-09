ffba951795f9b7bc2b4be8201741b574724823f2
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-01-09 11:16:35 -0500

    Cours 44- basic input and thruster

---
+++ 3_project_boost/Project boost/Assets/Scenes/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+            rigidBody.AddRelativeForce(Vector3.up);
+            
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Left key pressed");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Right key pressed");
+        }
+    }
+}
